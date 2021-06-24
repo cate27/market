1ff94788803ae9bcb227cca1156ad16dd8599e6f
cate27 <caterinar39@gmail.com>
2021-06-24 17:14:32 +0200

    aggiunta pagina di about

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   #controller pages
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get 'about' => 'pages#about'
+  
 end
