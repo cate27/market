3cdf11ad719def334076677f1bf89edc4d5338f2
cate27 <caterinar39@gmail.com>
2021-09-03 16:37:13 +0200

    validazione

--- app/models/content.rb
+++ app/models/content.rb
@@ -8,7 +8,9 @@ class Content < ActiveRecord::Base
 	has_attached_file :allegato
 	validates_attachment_content_type :allegato, :content_type => [/^image\/(png|gif|jpeg)/, 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/mspowerpoint', 'application/vnd.ms-powepoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/pdf', 'application/msexcel', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'audio/mpeg', 'audio/mp3' ],
 	message: "Formato non supportato"
-  
+	
+	validates :titolo, :descrizione, :price, presence: true
+	validates :price, numericality: { grater_than: 0 }
 end
 
 
